1239024987134cd3dd12889f820827b10c89fb9d
chitraeasiyanviji <91660192+chitraeasiyanviji@users.noreply.github.com>
2022-10-09 08:40:46 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+         const btn = document.querySelector('button');
+
+        var name = () => {
+
+             var name1 = document.getElementById("demo").value;
+             alert("hello!" + name); 
+             btn.style.cssText
+       
+
+     }
